98f12d4693906dea7f5922bab8f54c6f846d2242
bulat <melhi697@mail.ru>
2024-01-23 23:21:42 +0300

    Алгоритмическая Оптимизация

--- Sourse.cpp
+++ Sourse.cpp
@@ -36,16 +36,21 @@ void Documentation()
 }
 
 typedef struct Node {
-    double value;
+    float value;
     char operation;
     struct Node* pNext;
 }Node;
 
-int push(Node** top, double value, char operation) {
-    char opertion = 0;
+struct Point3D
+{
+    double x;
+    double y;
+    double z;
+};
+int push(Node** top, float value, char operation) {
     Node* pNew = (Node*)malloc(sizeof(Node));
-    pNew->value = value;
     pNew->operation = operation;
+    pNew->value = value;
     if (*top == NULL) {
         pNew->pNext = NULL;
         *top = pNew;
@@ -60,9 +65,14 @@ int pop(Node** top) {
     if (top == NULL) {
         return 0;
     }
+
     Node* tmp = *top;
+    char operation = tmp->operation;
+
     *top = tmp->pNext;
+
     free(tmp);
+
     return 0;
 }
 void CreateAxis()
@@ -127,104 +137,9 @@ void ControlAxis()
         y += yAlpha;
     if (GetKeyState(VK_LEFT) < 0)
         y -= yAlpha;
-    glRotatef(-y, 0, 0, 1);
+    glRotatef(y, 0, 0, 1);
 }
 
-bool Math(Node** top_n, Node** top_o)
-{
-    double a, b, c;
-    Node* tmp_n; Node* tmp_o;
-    tmp_n = *top_n;
-    tmp_o = *top_o;
-    a = tmp_n->value;
-    pop(top_n);
-    tmp_n = *top_n;
-    switch (tmp_o->operation)
-    {
-    case '+':
-
-        b = tmp_n->value;
-        c = a + b;
-        pop(top_o);
-        pop(top_n);
-        push(top_n, c, '0');
-        break;
-    case '-':
-        b = tmp_n->value;
-        c = b - a;
-        pop(top_o);
-        pop(top_n);
-        push(top_n, c, '0');
-        break;
-    case '*':
-        b = tmp_n->value;
-        c = a * b;
-        pop(top_o);
-        pop(top_n);
-        push(top_n, c, '0');
-        break;
-    case '/':
-        b = tmp_n->value;
-        if (a == 0)
-        {
-            printf("������ ������ �� ����!");
-            return false;
-        }
-        else {
-            c = b / a;
-            pop(top_o);
-            pop(top_n);
-            push(top_n, c, '0');
-            break;
-        }
-    case '^':
-        b = tmp_n->value;
-        c = pow(b, a);
-        pop(top_o);
-        pop(top_n);
-        push(top_n, c, '0');
-        break;
-    case 's':
-        c = sin(a);
-        pop(top_o);
-        push(top_n, c, '0');
-        break;
-    case 'c':
-        c = cos(a);
-        pop(top_o);
-        push(top_n, c, '0');
-        break;
-    case 't':
-        if (cos(a) == 0) {
-            printf("������� �� ����������");
-            return false;
-        }
-        c = tan(a);
-        pop(top_o);
-        push(top_n, c, '0');
-        break;
-    case 'e':
-        c = exp(a);
-        pop(top_o);
-        push(top_n, c, '0');
-        break;
-    case 'g':
-        if (sin(a) == 0) {
-            printf("��������� �� ����������");
-            return false;
-        }
-        c = 1 / tan(x);
-        pop(top_o);
-        push(top_n, c, '0');
-        break;
-    default:
-        printf("����������� �������!");
-        return false;
-        break;
-
-    }
-    return true;
-}
 int getRang(char Ch)
 {
     if (Ch == '+' || Ch == '-') return 1;
@@ -233,165 +148,258 @@ int getRang(char Ch)
     if (Ch == 's' || Ch == 'c' || Ch == 't' || Ch == 'g' || Ch == 'e') return 4;
     return 0;
 }
-
-struct Point3D
+int SortingStation(char* infixNotation, char* PostfixNotation)
 {
-    double x;
-    double y;
-    double z;
-};
-int parser(struct Point3D* Points)
-{
-    char operation = 0;
-    char Ch;
-    double value;
-    bool flag = 1;
-    Node* top_n = NULL;
-    Node* top_o = NULL;
+    char Ch = 0;
+    int i = 0, j = 0;//Счетчик строки input и output соответсвенно
 
+    Node* top = NULL;//указатель на вершину стека для операций
 
-    char function[100];
-    scanf("%s", function);
-
-    function[strlen(function)] = '\n';
-
-
-    int pointCount = 0;
-    for (double xParam = -8; xParam < 8; xParam += 0.15)
+    for (int i = 0; i < strlen(infixNotation);)
     {
-        for (double yParam = -8; yParam < 8; yParam += 0.15)
+        Ch = infixNotation[i];
+        if (Ch == ' ')
         {
-            for (int s = 0; s <= strlen(function); )
+            continue;
+        }
+        if (Ch == 'x')
+        {
+            PostfixNotation[j++] = Ch;
+            PostfixNotation[j++] = ' ';
+            i++;
+            continue;
+
+        }
+        if (Ch == 'y')
+        {
+            PostfixNotation[j++] = Ch;
+            PostfixNotation[j++] = ' ';
+            i++;
+            continue;
+
+        }
+        if (Ch == 's' || Ch == 'c' || Ch == 't' || Ch == 'e')
+        {
+            char chars[3];
+            for (int n = 0; n < 3; n++)
             {
-                Ch = function[s];
-                if (Ch == '\n')
-                    break;
-                if (Ch == ' ')
-                    s++;
-                if (Ch == 'x')
-                {
-                    push(&top_n, xParam, '0');
-                    s++;
-                    continue;
-                }
-                if (Ch == 'y')
-                {
-                    push(&top_n, yParam, '0');
-                    s++;
-                    continue;
-                }
-                if (Ch == 's' || Ch == 'c' || Ch == 't' || Ch == 'e')
-                {
-                    char chars[3];
-                    for (int n = 0; n < 3; n++)
-                    {
-                        Ch = function[s++];
-                        chars[n] = Ch;
-                    }
-                    if (chars[0] == 's' && chars[1] == 'i' && chars[2] == 'n')
-                    {
-                        push(&top_o, 0, 's');
-                        continue;
-                    }
-                    if (chars[0] == 'c' && chars[1] == 'o' && chars[2] == 's')
-                    {
-                        push(&top_o, 0, 'c');
-                        continue;
-                    }
-                    if (chars[0] == 't' && chars[1] == 'a' && chars[2] == 'n')
-                    {
-                        push(&top_o, 0, 't');
-                        continue;
-                    }
-                    if (chars[0] == 'c' && chars[1] == 't' && chars[2] == 'g')
-                    {
-                        push(&top_o, 0, 'g');
-                        continue;
-                    }
-                    if (chars[0] == 'e' && chars[1] == 'x' && chars[2] == 'p')
-                    {
-                        push(&top_o, 0, 'e');
-                        continue;
-                    }
-                }
-                if (Ch >= '0' && Ch <= '9')
-                {
-                    double value;
-                    char num[100] = { 0 };
-                    int j = 0;
-                    while (isdigit(function[s]) || function[s] == '.')
-                    {
-                        num[j] = function[s];
-                        s++;
-                        j++;
-                    }
-                    value = atof(num); // ����������� ������ � �����  � ����������� ���������� ���� float
-                    push(&top_n, value, '0');
-                    continue;
-                }
-                if (Ch == '+' || Ch == '-' || Ch == '*' || Ch == '/' || Ch == '^')
-                {
-                    float value = 0;
-                    char operation = 0;
-                    if (top_o != NULL && getRang(Ch) > getRang(top_o->operation))
-                    {
-                        push(&top_o, 0, Ch);
-                        s++;
-                        continue;
-                    }
-                    if (top_o != NULL && getRang(Ch) <= getRang(top_o->operation))
-                    {
-                        if (!Math(&top_n, &top_o))
-                        {
-                            system("pause");
-                            return 0;
-                        }
-                        continue;
-                    }
-                    if (top_o == 0) {
-                        push(&top_o, 0, Ch);
-                        s++;
-                        continue;
-                    }
-
-                }
-                if (Ch == '(') {
-
-                    push(&top_o, 0, Ch);
-
-                    s++;
-                    continue;
-                }
-                if (Ch == ')') {
-                    while (top_o->operation != '(') {
-                        if (!Math(&top_n, &top_o)) {
-                            system("pause");
-                            return 0;
-                        }
-                        else continue;
-                    }
-                    //�������� ������
-                    pop(&top_o);
-                    s++;
-                    continue;
-                }
-
+                Ch = infixNotation[i++];
+                chars[n] = Ch;
             }
-            while (top_o != NULL) {
-                Math(&top_n, &top_o);
-            }
-            if (top_n != NULL)
+            if (chars[0] == 's' && chars[1] == 'i' && chars[2] == 'n')
             {
-                struct Point3D newPoint;
-                newPoint.x = xParam;
-                newPoint.y = yParam;
-                newPoint.z = top_n->value;
-                Points[pointCount++] = newPoint;
-                pop(&top_n);
+                push(&top, NULL, 's');
+            }
+            else if (chars[0] == 'c' && chars[1] == 'o' && chars[2] == 's')
+            {
+                push(&top, NULL, 'c');
+            }
+            else if (chars[0] == 't' && chars[1] == 'a' && chars[2] == 'n')
+            {
+                push(&top, NULL, 't');
+            }
+            else if (chars[0] == 'c' && chars[1] == 't' && chars[2] == 'g')
+            {
+                push(&top, NULL, 'g');
+            }
+            continue;
+        }
+        if (Ch >= '0' && Ch <= '9')
+        {
+            while (isdigit(infixNotation[i]) || infixNotation[i] == '.')
+            {
+                Ch = infixNotation[i++];
+                PostfixNotation[j++] = Ch;
+            }
+            PostfixNotation[j++] = ' ';
+        }
+        if (Ch == '+' || Ch == '-' || Ch == '*' || Ch == '/' || Ch == '^')
+        {
+            float value = 0;
+            char operation = 0;
+            if (top != NULL && getRang(Ch) > getRang(top->operation))
+            {
+                push(&top, NULL, Ch);
+                i++;
+                continue;
+            }
+            if (top != NULL && getRang(Ch) <= getRang(top->operation))
+            {
+                PostfixNotation[j++] = top->operation;
+                PostfixNotation[j++] = ' ';
+                pop(&top);
+            }
+            if (top == NULL) {
+                push(&top, NULL, Ch);
+                i++;
+                continue;
+            }
+        }
+        if (Ch == '(') {
+
+            push(&top, NULL, Ch);
+            i++;
+            continue;
+        }
+        if (Ch == ')') {
+            while (top->operation != '(') {
+                PostfixNotation[j++] = top->operation;
+                PostfixNotation[j++] = ' ';
+                pop(&top);
+                if (top == NULL)
+                {
+                    printf("Скобки не согласованы");
+                    return -1;
+                }
+            }
+            pop(&top);//Выталкиваем открывыющую скобку
+            i++;
+            continue;
+        }
+
+    }
+    while (top != NULL)
+    {
+        PostfixNotation[j++] = top->operation;
+        PostfixNotation[j++] = ' ';
+        pop(&top);
+    }
+
+    return 0;
+}
+float Math(char* PostfixNotation, float xParam, float yParam)
+{
+    double a, b, c;
+
+    
+
+    Node* top = NULL;
+
+    char Ch = 0;
+    for (int i = 0; i < strlen(PostfixNotation); i++)
+    {
+        Ch = PostfixNotation[i];
+        if (Ch == ' ')
+        {
+            continue;
+        }
+        if (Ch == 'x')
+        {
+            push(&top, xParam, NULL);
+            continue;
+        }
+        if (Ch == 'y')
+        {
+            push(&top, yParam, NULL);
+            continue;
+        }
+        if (Ch >= '0' && Ch <= '9')
+        {
+            double value;
+            char num[100] = { 0 };
+            int j = 0;
+            while (isdigit(PostfixNotation[i]) || PostfixNotation[i] == '.')
+            {
+                num[j] = PostfixNotation[i];
+                i++;
+                j++;
+            }
+            value = atof(num);
+            push(&top, value, NULL);
+            continue;
+        }
+        if (Ch == '+' || Ch == '-' || Ch == '*' || Ch == '/' || Ch == '^')
+        {
+            switch (Ch)
+            {
+            case'+':
+                a = top->value;
+                pop(&top);
+                b = top->value;
+                pop(&top);
+                c = a + b;
+                push(&top, c, NULL);
+                break;
+            case'-':
+                a = top->value;
+                pop(&top);
+                b = top->value;
+                pop(&top);
+                c = b - a;
+                push(&top, c, NULL);
+                break;
+            case'*':
+                a = top->value;
+                pop(&top);
+                b = top->value;
+                pop(&top);
+                c = a * b;
+                push(&top, c, NULL);
+                break;
+            case'/':
+                a = top->value;
+                pop(&top);
+                b = top->value;
+                pop(&top);
+                if (a == 0)
+                {
+                    printf("На 0 делить нельзя!");
+                    return false;
+                }
+                c = b / a;
+                push(&top, c, NULL);
+                break;
+            case'^':
+                a = top->value;
+                pop(&top);
+                b = top->value;
+                pop(&top);
+                c = pow(b, a);
+                push(&top, c, NULL);
+                break;
+            }
+        }
+        if (Ch == 's' || Ch == 'c' || Ch == 't' || Ch == 'g')
+        {
+            switch (Ch)
+            {
+            case 's':
+                a = top->value;
+                pop(&top);
+                c = sin(a);
+                push(&top, c, NULL);
+                break;
+            case 'c':
+                a = top->value;
+                pop(&top);
+                c = cos(a);
+                push(&top, c, NULL);
+                break;
+            case 't':
+                a = top->value;
+                if (cos(a) == 0) {
+                    printf("tan(%d) Не существует", a);
+                    return false;
+                }
+                pop(&top);
+                c = tan(a);
+                push(&top, c, NULL);
+                break;
+            case 'g':
+                a = top->value;
+                if (sin(a) == 0) {
+                    printf("ctg(%d) Не существует", a);
+                    return false;
+                }
+                pop(&top);
+                c = 1 / tan(a);
+                push(&top, c, NULL);
+                break;
             }
         }
     }
-    return  pointCount;
+    float result = top->value;
+    return result;
 }
 int main()
 {
@@ -402,16 +410,26 @@ int main()
     Documentation();
 
 
+    char infixNotation[100] = { 0 }, PostfixNotation[100] = { 0 };
+    scanf("%s", infixNotation);
+
+    SortingStation(infixNotation, PostfixNotation);//Перевел инфиксную нотацию в постфиксную
 
 
-    struct Point3D Points[12000];
-    int pointCount = parser(Points);
+    struct Point3D Points[15000];
 
-
-
-
-
-
+    int pointCount = 0;
+    for (float xParam = -8; xParam < 8; xParam += 0.15)
+    {
+        for (float yParam = -8; yParam < 8; yParam += 0.15)
+        {
+            Point3D newPoint;
+            newPoint.x = xParam;
+            newPoint.y = yParam;
+            newPoint.z = Math(PostfixNotation, xParam, yParam);
+            Points[pointCount++] = newPoint;
+        }
+    }
 
 
     GLFWwindow* window;
@@ -440,7 +458,7 @@ int main()
 
         glPointSize(5);
 
-       
+
 
         glBegin(GL_POINTS);
         glColor3f(0, 0, 1);
